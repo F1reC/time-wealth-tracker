248759cb23df3b89a922caf275e2cf0c2791cdb2
F1reC <87696691+F1reC@users.noreply.github.com>
2024-11-25 06:04:36 +0800

    Initial Commit

---
+++ time-wealth-tracker/time_wealth_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  time_wealth_trackerApp.swift
+//  time-wealth-tracker
+//
+//  Created by F1reC on 25/11/24.
+//
+
+import SwiftUI
+
+@main
+struct time_wealth_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ time-wealth-trackerTests/time_wealth_trackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  time_wealth_trackerTests.swift
+//  time-wealth-trackerTests
+//
+//  Created by F1reC on 25/11/24.
+//
+
+import Testing
+
+struct time_wealth_trackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
